10dfdb1ffe98dc41da0da45f38e4ccd6b8dd3903
snore3372 <snore3372@example.com>
2025-08-24 05:38:52 +0000

    vault-crls

--- update-crls.sh
+++ update-crls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -s -o docs/nebula-vault-intermediate/certs/nebula-vault-intermediate.pem \
+curl -s -o docs/nebula-vault-intermediate/certs/nebula-vault-intermediate.cert.pem \
   https://vault-home.talha5389:8200/v1/pki_int/issuer/nebula-vault-intermediate/pem
 
 curl -s -o docs/nebula-vault-intermediate/crl/nebula-vault-intermediate.crl.pem \
